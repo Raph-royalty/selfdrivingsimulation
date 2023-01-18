725f8a6f6efd1bc6fa54dc0309b2c17369781006
raph_royalty <raphndiritu@gmail.com>
2023-01-18 12:40:46 +0300

    sensors

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car {
-    constructor(x,y,width,height){
+    constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -8,7 +8,7 @@ class Car {
         this.speed = 0;
         this.acceleration = 0.2;
         this.maxSpeed = 3;
-        this.friction = 0.05; 
+        this.friction = 0.05;
         this.angle = 0;
 
         this.sensor = new Sensor(this);
@@ -16,62 +16,97 @@ class Car {
         this.controls = new Controls();
     }
 
-    update(){
+    update(roadBorders) {
         this.#move();
-        this.sensor.update();
+        this.polygon = this.#createPolygon();
+        //this.sensor.update(roadBorders);
     }
-    #move(){
-        if(this.controls.forward){
-            this.speed+=this.acceleration;
+    #createPolygon() {
+        const points = [];
+        const rad = Math.hypot(this.width, this.height) / 2;
+        const alpha = Math.atan2(this.width, this.height);
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad
+        });
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad
+        });
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+        });
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+        });
+        return points;
+    }
+
+    #move() {
+        if (this.controls.forward) {
+            this.speed += this.acceleration;
         }
-        if(this.controls.reverse){
-            this.speed-=this.acceleration;
+        if (this.controls.reverse) {
+            this.speed -= this.acceleration;
         }
-        if(this.speed > this.maxSpeed){
+        if (this.speed > this.maxSpeed) {
             this.speed = this.maxSpeed;
         }
-        if(this.speed <- this.maxSpeed/2){
-            this.speed =- this.maxSpeed/2;
+        if (this.speed < - this.maxSpeed / 2) {
+            this.speed = - this.maxSpeed / 2;
         }
-        if(this.speed > 0){
-            this.speed -=this.friction;
+        if (this.speed > 0) {
+            this.speed -= this.friction;
         }
-        if(this.speed < 0){
-            this.speed +=this.friction;
+        if (this.speed < 0) {
+            this.speed += this.friction;
         }
-        if(Math.abs(this.speed) < this.friction){
+        if (Math.abs(this.speed) < this.friction) {
             this.speed = 0;
         }
-        if(this.speed != 0){
-            const flip = this.speed>0?1:-1;
-            if(this.controls.left){
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.left) {
                 //this.x -= 2;
                 this.angle += 0.03 * flip;
             }
-            if(this.controls.right){
+            if (this.controls.right) {
                 //this.x += 2;
                 this.angle -= 0.03 * flip;
             }
         }
-        this.x-= Math.sin(this.angle)*this.speed;
-        this.y-= Math.cos(this.angle)*this.speed;
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
         // this.y-=this.speed;
 
     }
-    draw(ctx){
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
-        ctx.beginPath();
-        ctx.rect(
-            - this.width/2,
-            - this.height/2,
-            this.width,
-            this.height
-        );
-        ctx.fill();
+    draw(ctx) {
+        // ctx.save();
+        // ctx.translate(this.x, this.y);
+        // ctx.rotate(-this.angle);
+        // ctx.beginPath();
+        // ctx.rect(
+        //     - this.width / 2,
+        //     - this.height / 2,
+        //     this.width,
+        //     this.height
+        // );
+        // ctx.fill();
+        //ctx.restore();
+        //ctx.beginPath();
 
-        ctx.restore();
+        
+
+        if (this.polygon) {
+            ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+            for (let i = 1; i < this.polygon.length; i++) {
+                ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+            }
+            ctx.fill();
+        }
+
 
         this.sensor.draw(ctx);
     }
